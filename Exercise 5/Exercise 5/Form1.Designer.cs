e199be45925ab5f61b55ebd44b7bab0b42ee33b2
joshhouk4 <70138603+joshhouk4@users.noreply.github.com>
2020-10-31 21:11:55 -0700

    Add files via upload
    Week 3 Exercise 5

---
+++ Exercise 5/Exercise 5/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Exercise_5
+{
+    partial class piForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(piForm));
+            this.termsLabel = new System.Windows.Forms.Label();
+            this.termsTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // termsLabel
+            // 
+            this.termsLabel.AutoSize = true;
+            this.termsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.termsLabel.Location = new System.Drawing.Point(12, 60);
+            this.termsLabel.Name = "termsLabel";
+            this.termsLabel.Size = new System.Drawing.Size(154, 24);
+            this.termsLabel.TabIndex = 0;
+            this.termsLabel.Text = "Enter # of Terms:";
+            // 
+            // termsTextBox
+            // 
+            this.termsTextBox.Location = new System.Drawing.Point(185, 60);
+            this.termsTextBox.Multiline = true;
+            this.termsTextBox.Name = "termsTextBox";
+            this.termsTextBox.Size = new System.Drawing.Size(177, 24);
+            this.termsTextBox.TabIndex = 1;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(16, 125);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(150, 61);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(16, 219);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(346, 85);
+            this.outputLabel.TabIndex = 3;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(212, 125);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(150, 61);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // piForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 341);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.termsTextBox);
+            this.Controls.Add(this.termsLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "piForm";
+            this.Text = "Approximate PI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label termsLabel;
+        private System.Windows.Forms.TextBox termsTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Exercise 5/Exercise 5/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_5
+{
+    public partial class piForm : Form
+    {
+
+        // My name is Joshua Houk and this is my Approximate PI program.
+        //
+        public piForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            int numberTerms;
+
+            if (int.TryParse(termsTextBox.Text, out numberTerms))
+            {
+                double pi = 4;
+
+                //Pi is 4 after only one term, so that is displayed here
+                if (numberTerms == 1)
+                {
+                    termsLabel.Text = "Approximate value of Pi after 1 term: ";
+                }
+                else if (numberTerms > 1 && numberTerms <= int.MaxValue)
+                {
+                    long denominator = 3;
+
+                    //false sets symbol to subtract, true sets symbol to add.
+                    bool addSubtract = false;
+
+                    for (int i = 2; i <= numberTerms; i++)
+                    {
+                        switch (addSubtract)
+                        {
+                            //false causes subtraction, true causes addition
+                            case false:
+                                pi = pi - ((double)4 / denominator);
+                                denominator += 2;
+                                addSubtract = !addSubtract;
+                                break;
+
+                            case true:
+                                pi = pi + ((double)4 / denominator);
+                                denominator += 2;
+                                addSubtract = !addSubtract;
+                                break;
+
+                        }
+                    }
+                }
+
+                outputLabel.Text = ("Approximate value of pi after ") + termsTextBox.Text + (" terms = ") + pi.ToString();
+            }
+            else
+            {
+                //Parse has failed.
+                MessageBox.Show("Invalid input for the number of terms. It must be a whole number between 1 and " + int.MaxValue.ToString());
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Closes the form.
+            this.Close();
+        }
+    }
+}
