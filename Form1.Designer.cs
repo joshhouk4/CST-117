3f0a0bfb923a4bfdb92189ad943fb9d0a4e4ac55
joshhouk4 <70138603+joshhouk4@users.noreply.github.com>
2020-11-07 22:56:33 -0700

    Milestone 2, CST117

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Inventory_System
+{
+    partial class manageInverntory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.partNumberLabel = new System.Windows.Forms.Label();
+            this.serialNumberLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.partNumberTextBox = new System.Windows.Forms.TextBox();
+            this.serialNumberTextBox = new System.Windows.Forms.TextBox();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(69, 60);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(106, 20);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Name of Part:";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // partNumberLabel
+            // 
+            this.partNumberLabel.AutoSize = true;
+            this.partNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.partNumberLabel.Location = new System.Drawing.Point(73, 120);
+            this.partNumberLabel.Name = "partNumberLabel";
+            this.partNumberLabel.Size = new System.Drawing.Size(102, 20);
+            this.partNumberLabel.TabIndex = 1;
+            this.partNumberLabel.Text = "Part Number:";
+            this.partNumberLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // serialNumberLabel
+            // 
+            this.serialNumberLabel.AutoSize = true;
+            this.serialNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serialNumberLabel.Location = new System.Drawing.Point(62, 180);
+            this.serialNumberLabel.Name = "serialNumberLabel";
+            this.serialNumberLabel.Size = new System.Drawing.Size(113, 20);
+            this.serialNumberLabel.TabIndex = 2;
+            this.serialNumberLabel.Text = "Serial Number:";
+            this.serialNumberLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(103, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Quantity:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTextBox.Location = new System.Drawing.Point(198, 60);
+            this.nameTextBox.Multiline = true;
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(301, 29);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // partNumberTextBox
+            // 
+            this.partNumberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.partNumberTextBox.Location = new System.Drawing.Point(198, 117);
+            this.partNumberTextBox.Multiline = true;
+            this.partNumberTextBox.Name = "partNumberTextBox";
+            this.partNumberTextBox.Size = new System.Drawing.Size(301, 29);
+            this.partNumberTextBox.TabIndex = 5;
+            // 
+            // serialNumberTextBox
+            // 
+            this.serialNumberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serialNumberTextBox.Location = new System.Drawing.Point(198, 180);
+            this.serialNumberTextBox.Multiline = true;
+            this.serialNumberTextBox.Name = "serialNumberTextBox";
+            this.serialNumberTextBox.Size = new System.Drawing.Size(301, 29);
+            this.serialNumberTextBox.TabIndex = 6;
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quantityTextBox.Location = new System.Drawing.Point(198, 240);
+            this.quantityTextBox.Multiline = true;
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(301, 29);
+            this.quantityTextBox.TabIndex = 7;
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(52, 292);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(195, 50);
+            this.addButton.TabIndex = 8;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(52, 360);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(195, 50);
+            this.clearButton.TabIndex = 9;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editButton.Location = new System.Drawing.Point(267, 292);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(195, 50);
+            this.editButton.TabIndex = 10;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(267, 360);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(195, 50);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // manageInverntory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 432);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.quantityTextBox);
+            this.Controls.Add(this.serialNumberTextBox);
+            this.Controls.Add(this.partNumberTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.serialNumberLabel);
+            this.Controls.Add(this.partNumberLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "manageInverntory";
+            this.Text = "Inventory Management";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label partNumberLabel;
+        private System.Windows.Forms.Label serialNumberLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox partNumberTextBox;
+        private System.Windows.Forms.TextBox serialNumberTextBox;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Item.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_System
+{
+    class Item
+    {
+        private string itemName;
+        private string itemPartNumber;
+        private string itemSerialNumber;
+        private string itemQuantity;
+
+        public string Name
+        {
+            get { return itemName; }
+            set { itemName = value; }
+        }
+
+        public string PartNumber
+        {
+            get { return itemPartNumber; }
+            set { itemPartNumber = value; }
+        }
+
+        public string SerialNumber
+        {
+            get { return itemSerialNumber; }
+            set { itemSerialNumber = value; }
+        }
+
+        public string Quantity
+        {
+            get { return itemQuantity; }
+            set { itemQuantity = value; }
+        }
+
+        public Item(string itemName, string itemPartNumber, string itemSerialNumber, string itemQuantity)
+        {
+            this.itemName = itemName;
+            this.itemPartNumber = itemPartNumber;
+            this.itemSerialNumber = itemSerialNumber;
+            this.itemQuantity = itemQuantity;
+        }
+
+        public Item()
+        {
+            itemName = "none";
+            itemPartNumber = "none";
+            itemSerialNumber = "none";
+            itemQuantity = "none";
+        }
+    }
+}
