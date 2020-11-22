1eb3f7863fa19f4d65d8f08b8fa265627fbbe2e7
joshhouk4 <70138603+joshhouk4@users.noreply.github.com>
2020-11-21 20:53:34 -0700

    CST-117-Programming-Project-4

---
+++ Programming Project 4/Programming Project 4/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Programming_Project_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.array00Label = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.array01Label = new System.Windows.Forms.Label();
+            this.array02Label = new System.Windows.Forms.Label();
+            this.array10Label = new System.Windows.Forms.Label();
+            this.array11Label = new System.Windows.Forms.Label();
+            this.array12Label = new System.Windows.Forms.Label();
+            this.array20Label = new System.Windows.Forms.Label();
+            this.array21Label = new System.Windows.Forms.Label();
+            this.array22Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // array00Label
+            // 
+            this.array00Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.array00Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.array00Label.Location = new System.Drawing.Point(76, 35);
+            this.array00Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.array00Label.Name = "array00Label";
+            this.array00Label.Size = new System.Drawing.Size(152, 102);
+            this.array00Label.TabIndex = 0;
+            this.array00Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(76, 368);
+            this.resultLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(464, 48);
+            this.resultLabel.TabIndex = 9;
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newGameButton.Location = new System.Drawing.Point(79, 432);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(180, 56);
+            this.newGameButton.TabIndex = 10;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(360, 432);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(180, 56);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // array01Label
+            // 
+            this.array01Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.array01Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.array01Label.Location = new System.Drawing.Point(232, 35);
+            this.array01Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.array01Label.Name = "array01Label";
+            this.array01Label.Size = new System.Drawing.Size(152, 102);
+            this.array01Label.TabIndex = 12;
+            this.array01Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // array02Label
+            // 
+            this.array02Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.array02Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.array02Label.Location = new System.Drawing.Point(388, 35);
+            this.array02Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.array02Label.Name = "array02Label";
+            this.array02Label.Size = new System.Drawing.Size(152, 102);
+            this.array02Label.TabIndex = 13;
+            this.array02Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // array10Label
+            // 
+            this.array10Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.array10Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.array10Label.Location = new System.Drawing.Point(76, 137);
+            this.array10Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.array10Label.Name = "array10Label";
+            this.array10Label.Size = new System.Drawing.Size(152, 102);
+            this.array10Label.TabIndex = 14;
+            this.array10Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // array11Label
+            // 
+            this.array11Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.array11Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.array11Label.Location = new System.Drawing.Point(232, 137);
+            this.array11Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.array11Label.Name = "array11Label";
+            this.array11Label.Size = new System.Drawing.Size(152, 102);
+            this.array11Label.TabIndex = 15;
+            this.array11Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // array12Label
+            // 
+            this.array12Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.array12Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.array12Label.Location = new System.Drawing.Point(388, 137);
+            this.array12Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.array12Label.Name = "array12Label";
+            this.array12Label.Size = new System.Drawing.Size(152, 102);
+            this.array12Label.TabIndex = 16;
+            this.array12Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // array20Label
+            // 
+            this.array20Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.array20Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.array20Label.Location = new System.Drawing.Point(76, 239);
+            this.array20Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.array20Label.Name = "array20Label";
+            this.array20Label.Size = new System.Drawing.Size(152, 102);
+            this.array20Label.TabIndex = 17;
+            this.array20Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // array21Label
+            // 
+            this.array21Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.array21Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.array21Label.Location = new System.Drawing.Point(232, 239);
+            this.array21Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.array21Label.Name = "array21Label";
+            this.array21Label.Size = new System.Drawing.Size(152, 102);
+            this.array21Label.TabIndex = 18;
+            this.array21Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // array22Label
+            // 
+            this.array22Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.array22Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.array22Label.Location = new System.Drawing.Point(388, 239);
+            this.array22Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.array22Label.Name = "array22Label";
+            this.array22Label.Size = new System.Drawing.Size(152, 102);
+            this.array22Label.TabIndex = 19;
+            this.array22Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 532);
+            this.Controls.Add(this.array22Label);
+            this.Controls.Add(this.array21Label);
+            this.Controls.Add(this.array20Label);
+            this.Controls.Add(this.array12Label);
+            this.Controls.Add(this.array11Label);
+            this.Controls.Add(this.array10Label);
+            this.Controls.Add(this.array02Label);
+            this.Controls.Add(this.array01Label);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.array00Label);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label array00Label;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label array01Label;
+        private System.Windows.Forms.Label array02Label;
+        private System.Windows.Forms.Label array10Label;
+        private System.Windows.Forms.Label array11Label;
+        private System.Windows.Forms.Label array12Label;
+        private System.Windows.Forms.Label array20Label;
+        private System.Windows.Forms.Label array21Label;
+        private System.Windows.Forms.Label array22Label;
+    }
+}
+
---
+++ Programming Project 4/Programming Project 4/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programming_Project_4
+{
+    public partial class Form1 : Form
+    {
+        int[,] arr;                                             // 2d int array
+        string[,] board;                                        // 2d string array
+
+        int xCount;                                             // track X count
+        int oCount;                                             // track O count
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            StartBoard();                                       // call method to start game
+        }
+
+        private void StartBoard()                               // method to initialize game, setup board and counter
+        {
+            arr = new int[3, 3];
+            board = new string[3, 3];
+            xCount = 0;
+            oCount = 0;
+        }
+
+        private void FillArray()                                // method to fill array arr with random numbers and store O or X to the array board
+        {
+            Random rand = new Random();                         // create a random object
+
+            for (int row = 0; row < 3; row++)                   // store random generated numbers in array index
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    arr[row, col] = rand.Next(0, 2);
+
+                    if (arr[row, col] == 0)                     // O = 0, X = 1
+                    {
+                        board[row, col] = "O";
+                        oCount++;
+                    }
+                    else
+                    {
+                        board[row, col] = "X";
+                        xCount++;
+                    }
+                }
+            }
+        }
+
+        private void SetGameTable()                             // method to set random array values to labels
+        {
+                                                                //display index of boards array in the lables
+            array00Label.Text = board[0, 0];
+            array01Label.Text = board[0, 1];
+            array02Label.Text = board[0, 2];
+            array10Label.Text = board[1, 0];
+            array11Label.Text = board[1, 1];
+            array12Label.Text = board[1, 2];
+            array20Label.Text = board[2, 0];
+            array21Label.Text = board[2, 1];
+            array22Label.Text = board[2, 2];
+
+        }
+
+        private void CheckForWinner()                          // method to check for winner
+        {
+            if (WinByRow("X"))
+            {
+                resultLabel.Text = "X Wins!";
+                return;
+            }
+            else if (WinByRow("O"))
+            {
+                resultLabel.Text = "O Wins!";
+                return;
+            }
+            else if (WinByColumn("X"))
+            {
+                resultLabel.Text = "X Wins!";
+                return;
+            }
+            else if (WinByColumn("O"))
+            {
+                resultLabel.Text = "O Wins!";
+                return;
+            }
+            else if (WinByDiagonal("X"))
+            {
+                resultLabel.Text = "X Wins!";
+                return;
+            }
+            else if (WinByDiagonal("O"))
+            {
+                resultLabel.Text = "O Wins!";
+                return;
+            }
+            else
+            {
+                resultLabel.Text = "Its a tie!";
+            }
+        }
+        private Boolean WinByRow(string charValue)              // method to check for row winner
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                if ((board[row, 0] == board[row, 1]) && (board[row, 0] == board[row, 2]))
+                {
+                    if (board[row, 0].Equals(charValue))
+                        return true;
+                }
+            }
+            return false;
+        }
+        private Boolean WinByColumn(string charValue)           // method to check for column winner
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                if ((board[0, col] == board[1, col]) && (board[1, col] == board[2, col]))
+                {
+                    if (board[col, 0].Equals(charValue))
+                        return true;
+                }
+            }
+            return false;
+        }
+        private Boolean WinByDiagonal(string charValue)         // method to check for diagonal winner
+        {
+
+            if ((board[0, 0] == board[1, 1]) && (board[1, 1] == board[2, 2]))       // check from left to to right diagonally
+            {
+                if (board[0, 0].Equals(charValue))
+                    return true;
+            }
+            if ((board[0, 2] == board[1, 1]) && (board[1, 1] == board[2, 0]))       // check from right to left diagonally
+            {
+                if (board[1, 1].Equals(charValue))
+                    return true;
+            }
+            return false;
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            do                                                  // loop to fill array and keep max count of X or O
+            {
+                StartBoard();
+                FillArray();
+                if ((oCount == 4 && xCount == 5) || (oCount == 5 && xCount == 4))
+                {
+                    break;
+                }
+            } while (true);
+            SetGameTable();                                     // call method to display board
+            CheckForWinner();                                   // call method that checks for winner
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();                                       // close the form
+        }
+    }
+}
