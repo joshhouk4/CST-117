3a0f23d6d8043d584ae9dae83a1509076c1abbaa
joshhouk4 <70138603+joshhouk4@users.noreply.github.com>
2020-11-29 22:01:20 -0700

    CST 117 Milestone 4

---
+++ Milestone 4/MainForm.Designer.cs
@@ -0,0 +1,388 @@
+﻿// My name is Joshua Houk and this is my Inventory Management System Project.
+// Git Repo:  https://github.com/joshhouk4/CST-117
+
+
+namespace Inventory_Management_System
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.addGroupBox = new System.Windows.Forms.GroupBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.partNumberTextBox = new System.Windows.Forms.TextBox();
+            this.quantTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.itemNameLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.productGroupBox = new System.Windows.Forms.GroupBox();
+            this.itemListBox = new System.Windows.Forms.ListBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.priceUpdateTextBox = new System.Windows.Forms.TextBox();
+            this.quantUpdateTextBox = new System.Windows.Forms.TextBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.pnLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.addGroupBox.SuspendLayout();
+            this.productGroupBox.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(82, 197);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add Item";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // addGroupBox
+            // 
+            this.addGroupBox.Controls.Add(this.addButton);
+            this.addGroupBox.Controls.Add(this.priceTextBox);
+            this.addGroupBox.Controls.Add(this.partNumberTextBox);
+            this.addGroupBox.Controls.Add(this.quantTextBox);
+            this.addGroupBox.Controls.Add(this.nameTextBox);
+            this.addGroupBox.Controls.Add(this.label3);
+            this.addGroupBox.Controls.Add(this.label1);
+            this.addGroupBox.Controls.Add(this.itemNameLabel);
+            this.addGroupBox.Controls.Add(this.label2);
+            this.addGroupBox.Location = new System.Drawing.Point(50, 41);
+            this.addGroupBox.Name = "addGroupBox";
+            this.addGroupBox.Size = new System.Drawing.Size(248, 237);
+            this.addGroupBox.TabIndex = 1;
+            this.addGroupBox.TabStop = false;
+            this.addGroupBox.Text = "Add Items";
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(114, 102);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.priceTextBox.TabIndex = 2;
+            // 
+            // partNumberTextBox
+            // 
+            this.partNumberTextBox.Location = new System.Drawing.Point(114, 60);
+            this.partNumberTextBox.Name = "partNumberTextBox";
+            this.partNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.partNumberTextBox.TabIndex = 4;
+            // 
+            // quantTextBox
+            // 
+            this.quantTextBox.Location = new System.Drawing.Point(114, 146);
+            this.quantTextBox.Name = "quantTextBox";
+            this.quantTextBox.Size = new System.Drawing.Size(100, 20);
+            this.quantTextBox.TabIndex = 3;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(114, 19);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Price:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Part Number:";
+            // 
+            // itemNameLabel
+            // 
+            this.itemNameLabel.AutoSize = true;
+            this.itemNameLabel.Location = new System.Drawing.Point(37, 22);
+            this.itemNameLabel.Name = "itemNameLabel";
+            this.itemNameLabel.Size = new System.Drawing.Size(38, 13);
+            this.itemNameLabel.TabIndex = 14;
+            this.itemNameLabel.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Quantity:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Enter Name or Part Number:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(58, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Price:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Quantity:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Part Number:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(54, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Name:";
+            // 
+            // productGroupBox
+            // 
+            this.productGroupBox.Controls.Add(this.itemListBox);
+            this.productGroupBox.Controls.Add(this.searchTextBox);
+            this.productGroupBox.Controls.Add(this.searchButton);
+            this.productGroupBox.Controls.Add(this.label4);
+            this.productGroupBox.Location = new System.Drawing.Point(304, 41);
+            this.productGroupBox.Name = "productGroupBox";
+            this.productGroupBox.Size = new System.Drawing.Size(280, 237);
+            this.productGroupBox.TabIndex = 15;
+            this.productGroupBox.TabStop = false;
+            this.productGroupBox.Text = "Product";
+            // 
+            // itemListBox
+            // 
+            this.itemListBox.FormattingEnabled = true;
+            this.itemListBox.Location = new System.Drawing.Point(6, 19);
+            this.itemListBox.Name = "itemListBox";
+            this.itemListBox.Size = new System.Drawing.Size(268, 147);
+            this.itemListBox.TabIndex = 18;
+            this.itemListBox.SelectedIndexChanged += new System.EventHandler(this.itemListBox_SelectedIndexChanged);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(135, 200);
+            this.searchTextBox.Multiline = true;
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(139, 26);
+            this.searchTextBox.TabIndex = 17;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(33, 200);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(84, 26);
+            this.searchButton.TabIndex = 16;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.priceUpdateTextBox);
+            this.groupBox2.Controls.Add(this.quantUpdateTextBox);
+            this.groupBox2.Controls.Add(this.updateButton);
+            this.groupBox2.Controls.Add(this.removeButton);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.pnLabel);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.nameLabel);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(590, 41);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(302, 237);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Selected Item";
+            // 
+            // priceUpdateTextBox
+            // 
+            this.priceUpdateTextBox.Location = new System.Drawing.Point(133, 102);
+            this.priceUpdateTextBox.Name = "priceUpdateTextBox";
+            this.priceUpdateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.priceUpdateTextBox.TabIndex = 20;
+            // 
+            // quantUpdateTextBox
+            // 
+            this.quantUpdateTextBox.Location = new System.Drawing.Point(133, 146);
+            this.quantUpdateTextBox.Name = "quantUpdateTextBox";
+            this.quantUpdateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.quantUpdateTextBox.TabIndex = 21;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(16, 197);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(133, 23);
+            this.updateButton.TabIndex = 22;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(155, 197);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(141, 23);
+            this.removeButton.TabIndex = 25;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // pnLabel
+            // 
+            this.pnLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnLabel.Location = new System.Drawing.Point(130, 60);
+            this.pnLabel.Name = "pnLabel";
+            this.pnLabel.Size = new System.Drawing.Size(100, 20);
+            this.pnLabel.TabIndex = 23;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nameLabel.Location = new System.Drawing.Point(130, 22);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(100, 20);
+            this.nameLabel.TabIndex = 24;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(765, 286);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 26;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // exportButton
+            // 
+            this.exportButton.Location = new System.Drawing.Point(664, 286);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(75, 23);
+            this.exportButton.TabIndex = 27;
+            this.exportButton.Text = "Export to .txt";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 331);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.productGroupBox);
+            this.Controls.Add(this.addGroupBox);
+            this.Name = "MainForm";
+            this.Text = "Inventory Management";
+            this.addGroupBox.ResumeLayout(false);
+            this.addGroupBox.PerformLayout();
+            this.productGroupBox.ResumeLayout(false);
+            this.productGroupBox.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.GroupBox addGroupBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.TextBox quantTextBox;
+        private System.Windows.Forms.TextBox partNumberTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label itemNameLabel;
+        private System.Windows.Forms.GroupBox productGroupBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.ListBox itemListBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox priceUpdateTextBox;
+        private System.Windows.Forms.TextBox quantUpdateTextBox;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Label pnLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button exportButton;
+    }
+}
+
